9c8b7d91458e44c9522cb6146b6f5eec724b53b3
erhant <erhany96@gmail.com>
2023-05-03 15:04:50 +0300

    setups for snarkjs

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
-# flutter_webview
+# SnarkJS within Flutter
 
-A new Flutter project.
+An attempt to use SnarkJS within Flutter WebView.
 
-## Getting Started
+- [WebView Tutorial](https://codelabs.developers.google.com/codelabs/flutter-webview#0)
+- [SnarkJS in Browser](https://github.com/iden3/snarkjs#in-the-browser)
 
-This project is a starting point for a Flutter application.
+The example circuit used within `multiplier_3` is simply a circuit to compute the multiplication of 3 numbers.
 
-A few resources to get you started if this is your first Flutter project:
+```json
+{
+  "inputSignals": {
+    "in": [3n, 7n, 11n]
+  },
+  "outputSignals" {
+    "out": 231
+  }
+}
+```
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+The provided prover key & verification key is of a PLONK proof system. PTAU used is for BN128 by [Polygon Hermez](powersOfTau28_hez_final_08.ptau) (less than $2^8$ constraints in this one).
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Disclaimer
 
-- `R` for hot-restart, `r` for hot-reload
+I have no idea of mobile development, this is practically my hello-world in the mobile space.
--- lib/src/menu.dart
+++ lib/src/menu.dart
@@ -1,9 +1,16 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 enum _MenuOptions {
   navigationDelegate,
   userAgent,
+  javascriptChannel,
+  loadFlutterAsset,
+  loadLocalFile,
+  loadHtmlString,
+  loadSnarkjs
 }
 
 class Menu extends StatefulWidget {
@@ -33,6 +40,32 @@ class _MenuState extends State<Menu> {
               content: Text('$userAgent'),
             ));
             break;
+          case _MenuOptions.javascriptChannel:
+            await widget.controller.runJavaScript('''
+var req = new XMLHttpRequest();
+req.open('GET', "https://api.ipify.org/?format=json");
+req.onload = function() {
+  if (req.status == 200) {
+    let response = JSON.parse(req.responseText);
+    SnackBar.postMessage("IP Address: " + response.ip);
+  } else {
+    SnackBar.postMessage("Error: " + req.status);
+  }
+}
+req.send();''');
+            break;
+          case _MenuOptions.loadFlutterAsset:
+            await _onLoadFlutterAssetExample(widget.controller, context);
+            break;
+          case _MenuOptions.loadLocalFile:
+            await _onLoadLocalFileExample(widget.controller, context);
+            break;
+          case _MenuOptions.loadHtmlString:
+            await _onLoadHtmlStringExample(widget.controller, context);
+            break;
+          case _MenuOptions.loadSnarkjs:
+            await _onLoadSnarkjsExample(widget.controller, context);
+            break;
         }
       },
       itemBuilder: (context) => [
@@ -44,7 +77,91 @@ class _MenuState extends State<Menu> {
           value: _MenuOptions.userAgent,
           child: Text('Show user-agent'),
         ),
+        const PopupMenuItem<_MenuOptions>(
+          value: _MenuOptions.javascriptChannel,
+          child: Text('Lookup IP Address'),
+        ),
+        const PopupMenuItem<_MenuOptions>(
+          value: _MenuOptions.loadFlutterAsset,
+          child: Text('Load Flutter Asset'),
+        ),
+        const PopupMenuItem<_MenuOptions>(
+          value: _MenuOptions.loadHtmlString,
+          child: Text('Load HTML string'),
+        ),
+        const PopupMenuItem<_MenuOptions>(
+          value: _MenuOptions.loadLocalFile,
+          child: Text('Load local file'),
+        ),
+        const PopupMenuItem<_MenuOptions>(
+          value: _MenuOptions.loadSnarkjs,
+          child: Text('Load SnarkJS example'),
+        ),
       ],
     );
   }
+}
+
+Future<void> _onLoadFlutterAssetExample(
+    WebViewController controller, BuildContext context) async {
+  await controller.loadFlutterAsset('assets/www/demo.html');
+}
+
+Future<void> _onLoadLocalFileExample(
+    WebViewController controller, BuildContext context) async {
+  final String pathToIndex = await _prepareLocalFile();
+
+  await controller.loadFile(pathToIndex);
+}
+
+Future<void> _onLoadSnarkjsExample(
+    WebViewController controller, BuildContext context) async {
+  await controller.loadFlutterAsset('assets/www/snarkjs.html');
+}
+
+Future<String> _prepareLocalFile() async {
+  final String tmpDir = (await getTemporaryDirectory()).path;
+  final File indexFile = File('$tmpDir/www/demo.html');
+
+  await Directory('$tmpDir/www').create(recursive: true);
+  await indexFile.writeAsString('''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Load file example</title>
+</head>
+<body>
+
+<h1>Local demo page</h1>
+<p>
+ This is an example page used to demonstrate how to load a local file or HTML
+ string using the <a href="https://pub.dev/packages/webview_flutter">Flutter
+ webview</a> plugin.
+</p>
+
+</body>
+</html>
+''');
+
+  return indexFile.path;
+}
+
+Future<void> _onLoadHtmlStringExample(
+    WebViewController controller, BuildContext context) async {
+  await controller.loadHtmlString('''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Load HTML string example</title>
+</head>
+<body>
+
+<h1>Local demo page</h1>
+<p>
+ Tiny paragraph.
+</p>
+
+</body>
+</html>
+''');
 }
